153e4b8941d2e81999e33f7eecbaac6a9d5a1aaa
QuakeToEarth <73459097+QuakeToEarth@users.noreply.github.com>
2021-09-26 11:53:15 +1000

    Add files via upload

---
+++ CloudStorage/assignment.py
@@ -0,0 +1,7 @@
+import dropbox
+
+token = "sl.A5MECk15ulV0WUyNcSTWItAWIqX6Ms4n4b73zkOu-9PDQR8Zv7dTCcOBWvv3Z3J-JUQWd0qZAP0_bjnCNY8yJlfiYWPSExZoOxX-d3sns7G6ApE9ag2RPYBTkCHfOowjqRDuIkwozok"
+dbx = dropbox.Dropbox(token)
+
+f = open("textA.txt", "rb")
+dbx.files_upload(f.read(),"/AliciaCloud/textA.txt")
